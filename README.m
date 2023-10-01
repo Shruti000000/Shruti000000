84b56d7b6ac22a8520a6d6a7a33b63321db29641
Shruti000000 <146639245+Shruti000000@users.noreply.github.com>
2023-10-01 19:34:15 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Shruti000000
+- 👀 I’m interested in .health analysis..
+- 🌱 I’m currently learning ..data analytics.
+- 💞️ I’m looking to collaborate on ..data analysis.
+- 📫 How to reach me ...
+
+<!---
+Shruti000000/Shruti000000 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
